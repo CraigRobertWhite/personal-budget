a509e48c5eb579a60d24fb08e3a55114d88337c6
Craig White <craigwhiteofficial@gmail.com>
2020-10-29 19:27:26 -0400

    Added Mongoose to project

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+// DATABASE ------------------------------------------------------------------------------------------------------------
+
+mongoose.connect('mongodb://localhost:27017/quiz_8', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Routes --------------------------------------------------------------------------------------------------------------
 
 app.use('/', express.static('public'));
 
